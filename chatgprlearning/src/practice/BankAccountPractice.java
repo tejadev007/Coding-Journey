60834f01ab684762e64d9165c94b16bd0fb4e54f
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-08-13 04:35:50 -0400

    Revision Day 6

---
+++ chatgprlearning/src/practice/BankAccountPractice.java
@@ -0,0 +1,25 @@
+package practice;
+
+public class BankAccountPractice {
+	private String accountNumber;
+	private double balance;
+	
+	public BankAccountPractice(String accountNumber, double initialBalance) {
+		this.accountnumber = accountNumber;
+		this.balance = initialBalance;
+	}
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double amount) {
+		if(amount > 0) {
+			
+		}
+	}
+}
