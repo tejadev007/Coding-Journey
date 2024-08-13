7e51e250b242d7aca51eaaa11bacb3785b8a6e1c
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-08-13 04:48:42 -0400

    Revision Day 6

--- chatgprlearning/src/practice/MainForBankAccountPractice.java
+++ chatgprlearning/src/practice/MainForBankAccountPractice.java
@@ -3,5 +3,14 @@ package practice;
 public class MainForBankAccountPractice {
 	public static void main(String[] args) {
 		BankAccountPractice BAP= new BankAccountPractice("20029", 200034);
+		
+		System.out.println("Account number is "+BAP.getAccountNumber());
+		System.out.println("Account Balance is $ "+BAP.getBalance() );
+		
+		BAP.deposit(20030);
+		System.out.println("Account number is "+BAP.getBalance());
+		
+		BAP.withdraw(2034);
+		System.out.println("Account number is "+BAP.getBalance());
 	}
 }
